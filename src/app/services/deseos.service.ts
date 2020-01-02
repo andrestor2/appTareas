fa7f103558df9f0e1b096b99ebbd912af97a50a6
Andres Felipe Torres <andres.torres@ceiba.com.co>
2020-01-01 23:18:36 -0500

    Clase 122: Se agregar presistencia (LocalStorage)

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -9,15 +9,25 @@ export class DeseosService {
   public listas: Lista[] = [];
 
   constructor() {
-
-    const lista1 = new Lista('Recolectar piedras infinito');
-    const lista2 = new Lista('Heroes a desaparecer');
-
-    this.listas.push(lista1, lista2);
+    this.cargarStorage();
   }
 
   crearLista(titulo: string) {
     const nuevaLista = new Lista(titulo);
     this.listas.push(nuevaLista);
+    this.guardarStorage();
+  }
+
+  guardarStorage() {
+    localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+
+  cargarStorage() {
+    const data = localStorage.getItem('data');
+
+    if (data) {
+      this.listas = JSON.parse(localStorage.getItem('data'));
+    }
+
   }
 }
